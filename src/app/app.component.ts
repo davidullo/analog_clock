38e469ccca03554783799257adc84a9b5de57190
davidullo <davbarbi@gmail.com>
2024-01-23 23:10:02 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clock';
+
+  d!: Date;
+  hr!: number;
+  min!: number;
+  sec!: number;
+  hr_rotation!: number;
+  min_rotation!: number;
+  sec_rotation!: number;
+
+  ngOnInit() {
+    this.startClock();
+  }
+
+  startClock() {
+    setInterval(() => {
+      this.d = new Date();
+      this.hr = this.d.getHours();
+      this.min = this.d.getMinutes();
+      this.sec = this.d.getSeconds();
+      this.hr_rotation = 30 * this.hr + this.min / 2;
+      this.min_rotation = 6 * this.min;
+      this.sec_rotation = 6 * this.sec;
+    }, 1000);
+  }
 }
